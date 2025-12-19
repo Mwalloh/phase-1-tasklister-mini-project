9f92a136e868263d8430a47559ef536423edb1e1
Jim Ochieng <jimochieng64@gmail.com>
2025-12-19 19:23:51 +0300

    Update JS file to enable user to add items in TODO list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const formInput = document.getElementById("create-task-form");
+  formInput.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const taskInput = event.target["new-task-description"].value;
+    const task = {
+      description: taskInput,
+    };
+
+   buildToDo(task);
+  });
+
+  function buildToDo(task) {
+    const li = document.createElement("li");
+    li.textContent = task.description;
+
+    const taskList = document.getElementById("tasks");
+    taskList.appendChild(li);
+   }; 
 });
